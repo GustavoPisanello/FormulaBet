da443cc5c68cb29fcd7731018746c24b76f73f81
Gustavo Pisanello <gustavo.pisanello@etec.sp.gov.br>
2024-06-11 13:43:18 -0300

    Home e Nav/Menu finalizados

---
+++ public/assets/scripts/menu.js
@@ -0,0 +1,22 @@
+let menuLateral = document.querySelector(".menu-lateral");
+let etiqueta = document.querySelectorAll(".etiqueta");
+
+function AbreMenu(){
+    if(menuLateral.classList.contains("menuAberto")){
+        menuLateral.classList.remove("menuAberto");
+        menuLateral.classList.add("menuFechado");
+        
+        for (i = 0; i < 3; i++){
+            etiqueta[i].classList.toggle("etiqueta")
+        }
+    }
+    else{
+        menuLateral.classList.remove("menuFechado");
+        menuLateral.classList.add("menuAberto");
+        
+        for (i = 0; i < 3; i++){
+            etiqueta[i].classList.toggle("etiqueta")
+        }
+        
+    }
+}
